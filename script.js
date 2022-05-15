2ddd95f759ff7be366fbad3d3c75db630207c73b
Rohit <rohitrajvns2020@gmail.com>
2022-05-16 00:07:25 +0530

    added clock ticking audio

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-setInterval(() =>{
+setInterval(() => {
     d = new Date();
     htime = d.getHours();
     mtime = d.getMinutes();
     stime = d.getSeconds();
 
-    hrot = 30*htime + mtime/2 + stime/120;
-    mrot = 6*mtime;
-    srot = 6*stime;
+    hrot = 30 * htime + mtime / 2 + stime / 120;
+    mrot = 6 * mtime;
+    srot = 6 * stime;
 
     hour.style.transform = `rotate(${hrot}deg)`;
     minute.style.transform = `rotate(${mrot}deg)`;
     second.style.transform = `rotate(${srot}deg)`;
 
 }, 500);
+
+let audio = new Audio('Assets/Tick-tock.wav');
+let music = true;
+
+if(music == true){
+    audio.playbackRate = 0.5;
+    audio.play();
+    audio.loop = true;
+}
